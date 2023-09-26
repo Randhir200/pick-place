43184caac088359f2409788b148fa4ee8af2a464
Randhir Kumar <randhirk@lentra.ai>
2023-09-26 16:57:54 +0530

    modified directory

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const logger = require('./logger.js');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+logger.log('error', 'Something wrong with request Payload');
